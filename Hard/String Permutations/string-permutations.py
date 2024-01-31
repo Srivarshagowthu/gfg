089b0b0b88a70cef76be633a7ffbed4d1f77dfb4
Srivarsha gowthu <103606595+Srivarshagowthu@users.noreply.github.com>
2024-01-31 15:45:26 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/String Permutations/string-permutations.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    def permutation(self,s):
+        def perm(s,ds,ans,freq):
+            if len(ds)==len(s):
+                f="".join(ds)
+                res.append(f)
+                return
+            for i in range(len(s)):
+                if freq[i]==0:
+                    ds.append(s[i])
+                    freq[i]=1
+                    perm(s,ds,ans,freq)
+                    freq[i]=0
+                    ds.pop()
+        res=[]
+        ds=[]
+        n=len(s)
+        freq=[0]*len(s)
+        ans=[]
+        perm(s,ds,ans,freq)
+        return sorted(res)
+        
+                    
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    T=int(input())
+    while(T>0):
+        s=input()
+        ob=Solution()
+        ans=ob.permutation(s)
+        for i in ans:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+# } Driver Code Ends
