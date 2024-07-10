2caa003c8db7055eeb80021dd30f1de38e121015
Srivarsha gowthu <103606595+Srivarshagowthu@users.noreply.github.com>
2024-07-10 13:51:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.py
+++ Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.py
@@ -9,58 +9,45 @@ class LinkedList:
         self.tail=None
         '''
 class Solution:
-    
-    #Function to count nodes of a linked list.
-    def getCount(self, head_node):
-        #code here
-        if head_node is not None:
-            cur=head_node
-            c=0
-            while(cur!=None):
-                c+=1
-                cur=cur.next
-            return c
-        else:
+    # Function to count nodes of a linked list.
+    def getCount(self, head):
+        # code here
+        c=0
+        if head is None:
             return 0
-        
+        cur=head
+        while(cur):
+            c+=1
+            cur=cur.next
+        return c
+
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-import io
-import sys
 
-    
-# Node class
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-#Linked list class
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.tail=None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-    # append at the end of the list
-    def append(self,new_node):
-        if self.head is None:
-            self.head=new_node
-            self.tail=new_node
-            return
-        self.tail.next=new_node
-        self.tail = self.tail.next
 
-if __name__ == '__main__':
-    t=int(input())
-    for cases in range(t):
-        n=int(input())
-        a=LinkedList()
-        nodes=list(map(int,input().strip().split())) #list containing nodes
-        for x in nodes:
-            node=Node(x) # create a new node
-            a.append(node)
-        print(Solution().getCount(a.head))
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+
+    t = int(data[0])
+    for i in range(1, t + 1):
+        arr = list(map(int, data[i].split()))
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        ob = Solution()
+        print(ob.getCount(head))
+
 # } Driver Code Ends
